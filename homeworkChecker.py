fcf97d98d8966701c669eadf2505a2fd7bd4728c
Suitcase Class <suitcaseclass@gmail.com>
2017-05-16 08:40:12 +0000

    Updated with fuzzy string matching

--- homeworkChecker.py
+++ homeworkChecker.py
@@ -13,17 +13,103 @@ Dependencies: use python3 -m pip install [package1] [package2] [...]
     matplotlib
     pandas
     scipy
+    fuzzywuzzy
 
-Example: python3 -m pip install numpy datascience matplotlib pandas scipy
+Example: python3 -m pip install numpy datascience matplotlib pandas scipy fuzzywuzzy
 """
 
 import sys
 import numpy as np
 from datascience import *
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
+
+def find_fuzzy_matches(all_emails, submission_emails):
+    """Given an array of all emails and an array of submission emails, uses fuzzy string matching to find all emails that did not submit
+    
+    ============Suite 1============
+    >>> all_emails        = ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu', 'oski@berkeley.edu']
+    >>> submission_emails = ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu', 'oski@berkeley.edu']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    []
+    >>> submission_emails = ['kunalmishra9@gmial', 'kmishra9@berkeley.edu', 'oski@berkeley.edu']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    []
+    >>> submission_emails = ['kunalmishra9@gmial ', 'kmishra9@berkeley', 'oski@berkeley.edu ']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    []
+    
+    ============Suite 2============
+    >>> all_emails        = ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu', 'oski@berkeley.edu']
+    >>> submission_emails = ['oski@berkeley.edu ']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu']
+    >>> submission_emails = ['kmishra9@berkeley ','oski@berkeley.edu ']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    ['kunalmishra9@gmail.com']
+    
+    ============Suite 3============
+    >>> all_emails        = ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu', 'oski@berkeley.edu']
+    >>> submission_emails = ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu', 'oski@berkeley.edu', 'rando@berkeley']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    []
+    >>> submission_emails = ['kunalmishra9@gmail.com', 'oski@berkeley.edu', 'rando@berkeley']
+    >>> find_fuzzy_matches(all_emails, submission_emails)
+    **********
+    Please check on kmishra9@berkeley.edu. The most similar email we found in the submissions was rando@berkeley with a similarity score of 86 out of 100. 
+    **********
+    ['kmishra9@berkeley.edu']
+    """
+    
+    num_students, num_submissions = len(all_emails), len(submission_emails)
+    
+    matches = [ process.extract(query=student_email, choices=submission_emails) + [(student_email, -1)] for student_email in all_emails ] 
+    
+    #Removes all perfect matches -- need to take the top X matches, where X = submissions - perfect matches
+    get_top_similarity_score = lambda processed_query: processed_query[0][1]
+    get_most_similar_email   = lambda processed_query: processed_query[0][0]
+    get_email                = lambda processed_query: processed_query[-1][0]
+
+    
+    missing_submissions = [processed_query for processed_query in matches if get_top_similarity_score(processed_query) != 100]
+    num_missing_submissions = len(missing_submissions)
+    
+    #False negatives -- people inputted their email incorrectly
+    if num_missing_submissions + num_submissions > num_students:
+        num_false_negatives = num_missing_submissions + num_submissions - num_students 
+        
+        missing_submissions = sorted( missing_submissions, key=lambda processed_query: processed_query[0][1], reverse=True )
+        
+        
+        count = 0
+        flagged = []
+        for missing_submission in missing_submissions:
+            if (get_top_similarity_score(missing_submission) < 80 or get_most_similar_email(missing_submission)[0] != get_email(missing_submission)[0]) and count < num_false_negatives:
+                print("**********" + "\nPlease check on " + get_email(missing_submission) + ". The most similar email we found in the submissions was " +
+                get_most_similar_email(missing_submission) + " with a similarity score of " + str(get_top_similarity_score(missing_submission)) + " out of 100.", "\n**********" )
+                
+                flagged.append(missing_submission)
+                
+            count += 1
+        
+        #Getting rid of false negatives (people who were the closest fuzzy matches)
+        missing_submissions = missing_submissions[num_false_negatives:] + flagged
+        
+    #Logical error or issue with input files    
+    elif num_missing_submissions + num_submissions < num_students:
+        error_msg =  "Something went wrong -- most likely, your roster is incomplete or a student submitted twice, please correct the input files\n\n"
+        error_msg += "Here are the students with 'missing' submissions' " + str(missing_submissions) 
+        assert False, error_msg
+    
+        
+    
+    missing_submissions = [get_email(processed_query) for processed_query in missing_submissions]
+    
+    return missing_submissions
 
 #Ensuring that enough file paths have been passed into the script
-improper_arg_msg = "Improper arguments passed in - 'python3 homeworkChecker.py [roster filename] [homework 1 responses filename] [...]'"
+improper_arg_msg = "Usage: 'python3 homeworkChecker.py [roster filename] [homework 1 responses filename] [...]'"
 assert len(sys.argv) >= 3, improper_arg_msg
 
 #Getting the roster and homework response paths
@@ -46,8 +132,7 @@ students_without_submissions = []
 
 for homework_table in homework_tables:
     submitted_student_emails = set( homework_table.column("Email") )
-
-    students_without_submissions.append( all_student_emails - submitted_student_emails )
+    students_without_submissions.append( find_fuzzy_matches(all_student_emails, submitted_student_emails) )
 
 #Figuring out the number of times each student missed a homework
 missed_homeworks = dict()       #Maps from student's email -> # of missed homeworks
@@ -83,3 +168,7 @@ for email in emails[1:]:
     file.write( email )
 
 file.close()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
