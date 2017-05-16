0f4a620d3f01488f93ea75e8e3907c61196b57bf
Suitcase Class <suitcaseclass@gmail.com>
2017-05-16 09:21:39 +0000

    Began work on automatic email notification

---
+++ notifyByEmail.py
@@ -0,0 +1,20 @@
+"""
+Python Script designed to notify all students who did not complete the homework
+
+usage: python3 notifyByEmail.py [students_without_submissions filename]
+
+example: python3 notifyByEmail.py students_without_submissions.txt
+
+Given a file containing a set of emails for students who are missing submissions, emails them a generic message letting them know we didn't receive the homework and that they need to complete the HW ASAP.
+
+Dependencies: use python3 -m pip install [package1] [package2] [...]
+    TBD
+
+Example: python3 -m pip install TBD
+
+
+Resources
+    http://naelshiab.com/tutorial-send-email-python/
+    https://docs.python.org/3/library/email-examples.html
+"""
+
