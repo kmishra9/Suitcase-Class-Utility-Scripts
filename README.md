d97870451d208d3e78219b066e26ffded769dfed
Suitcase Class <suitcaseclass@gmail.com>
2017-05-27 22:06:24 +0000

    Significant progress on automated email notifications

---
+++ README.md
@@ -0,0 +1,8 @@
+Hi there! Check out the Documentation Overview by visiting this link: 
+
+https://goo.gl/UF48BR
+
+If you need any help or have any questions, my quick contact info is also here:
+
+Email: kunalmishra9@gmail.com
+Facebook Messenger: https://www.facebook.com/kunalmishra9
--- notifyByEmail.py
+++ notifyByEmail.py
@@ -18,3 +18,43 @@ Resources
     https://docs.python.org/3/library/email-examples.html
 """
 
+import smtplib
+from email.mime.text import MIMEText
+import termcolor
+
+def get_recipients():
+    return ['kunalmishra9@gmail.com', 'kmishra9@berkeley.edu']
+
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+user_email, user_pass = 'suitcaseclass@gmail.com', 'sweetcaseclass!'
+server.login(user_email, user_pass)                                             #Fixes any login issues: https://accounts.google.com/DisplayUnlockCaptcha
+
+homework_number    = input("What homework number are you emailing about?\n").strip()
+assert 0 <= int(homework_number) <= 16, 'Invalid homework number inputted -- should be an integer between 0 and 16. Quitting...'
+CD_initials        = input("What are the CD Initials for this semester?\n").strip().upper()
+assert len(CD_initials) == 4
+
+recipients = get_recipients()
+
+for recipient in recipients:
+
+    #Example email body: https://docs.google.com/document/d/1ZIb9yZK-MxvVsqHYSn7jzIarDoOGT6oZ-0_9DRtYM4M/edit?usp=sharing
+    msg = "Hey y'all,\n\n"
+    msg+= "Just reaching out to let you know we didn't receive a HW" +homework_number+ " submission from you.\n\n"
+    msg+= "If you think this is a mistake, please let us know. Otherwise, please complete last week's homework as soon as you can.\n\n"
+    msg+= CD_initials
+    
+    msg = MIMEText(msg)
+    
+    msg['Subject'] = '[Action Required] HW' + homework_number + ' Submission Missing'
+    msg['From']    = user_email
+    msg['To']      = recipient
+
+    server.send_message(msg)
+    
+print("Reminder emails have been sent!")
+
+server.quit()
